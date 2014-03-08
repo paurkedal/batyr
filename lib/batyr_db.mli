6fa36506e358edcb77e1e50dcc2ce553bbf45151
Petter Urkedal <paurkedal@gmail.com>
2014-03-08 09:13:49 +0100

    Batyr_db: Expose Runtime_error.

--- lib/batyr_db.mli
+++ lib/batyr_db.mli
@@ -1,4 +1,4 @@
-(* Copyright (C) 2013  Petter Urkedal <paurkedal@gmail.com>
+(* Copyright (C) 2013--2014  Petter Urkedal <paurkedal@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+exception Runtime_error of string
 exception Response_error of string
 
 val or_null : string option -> string
