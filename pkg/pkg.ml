b8ada69344d3bf38fe11c4a64969465c75567818
Petter A. Urkedal <paurkedal@gmail.com>
2022-06-15 21:46:38 +0200

    Remove obsolete build and packaging files.

--- pkg/pkg.ml
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env
-#use "topfind"
-#require "topkg-jbuilder"
-
-open Topkg
-
-let licenses = List.map Pkg.std_file ["COPYING"]
-
-let () = Topkg_jbuilder.describe ~licenses ~name:"batyr-core" ()
